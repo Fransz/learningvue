b18a3953e2232b0b658f7164e39dfed421e1157e
Frans Jaspers <fjaspers@xs4all.nl>
2024-04-13 11:39:46 +0200

    Setting up the jobboard project

---
+++ src/components/jobboard/JobBoard.vue
@@ -0,0 +1,8 @@
+<script lang="ts"></script>
+
+<template>
+  <h1>JobBoard</h1>
+  <div>Hello World</div>
+</template>
+
+<script scoped></script>
---
+++ src/components/jobboard/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+
+import JobBoard from './JobBoard.vue';
+
+const app = createApp(JobBoard);
+app.mount('#app');
